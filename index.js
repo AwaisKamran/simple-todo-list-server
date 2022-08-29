6bf47f1a4e7ee18218f720ee3b4cf49702a660c3
Awais Kamran <awais.kamran@taxfix.de>
2022-08-29 10:06:22 +0500

    feat: endpoints updated.

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 const PORT = 8081;
 
-app.get("/", (req, res) => {
-  res.send("hello world!");
+let list = [
+  {
+    id: 1,
+    completed: false,
+    userId: 1,
+    title: "hello world",
+  },
+];
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/todos", (req, res) => {
+  res.json(list);
+});
+
+app.post("/todos", (req, res) => {
+  list.push(req.body);
+  res.sendStatus(200);
 });
 
 app.listen(PORT, () => {
