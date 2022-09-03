777cb726504020edec86c13aed408a373cbbc7e7
Awais Kamran <awais.kamran@taxfix.de>
2022-09-03 17:27:02 +0500

    feat: update and delete endpoints added.

--- index.js
+++ index.js
@@ -17,13 +17,45 @@ let list = [
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/todos", (req, res) => {
-  res.json(list);
-});
-
+//Create
 app.post("/todos", (req, res) => {
   list.push(req.body);
   res.sendStatus(200);
+});
+
+//Read
+app.get("/todos", (req, res) => {
+  res.json(list).sendStatus(200);
+});
+
+//Update
+app.put("/todos/:id", (req, res) => {
+  const { id } = req.params;
+  const formattedId = parseInt(id);
+
+  if (!isNaN(formattedId)) {
+    list.forEach((item) => {
+      if (item.id === formattedId) {
+        item.completed = !item.completed;
+        res.status(200).json({ success: true, msg: "Item has been updated" });
+      }
+    });
+  } else {
+    res.status(400).json({ error: "Invalid Parameter" });
+  }
+});
+
+//Delete
+app.delete("/todos/:id", (req, res) => {
+  const { id } = req.params;
+  const formattedId = parseInt(id);
+
+  if (!isNaN(formattedId)) {
+    list = list.filter((item) => item.id !== formattedId);
+    res.sendStatus(200);
+  } else {
+    res.status(400).json({ error: "Invalid Parameter" });
+  }
 });
 
 app.listen(PORT, () => {
